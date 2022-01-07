d66454d8aab5e9252965f10029ba0d892eb62822
Mark Jia <mjia@ualberta.ca>
2022-01-07 14:28:55 -0700

    gui support

---
+++ guiTest.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root_window = Tk()
+
+lab_welcom = Label(root_window, text= "Welcome to the Room Booking System!")
+ 
+availible_rooms = pass
+
+
+
+
+
+
+root_window.mainloop()
